b92adf2e43099de0499a5a30dd1159a65c4f1018
ddisanto-langops <1daniel.disanto@gmail.com>
2026-03-10 19:08:36 -0400

    Fix errors in server entry point

--- server/server.mjs
+++ server/server.mjs
@@ -1,22 +1,22 @@
 import express from 'express';
 import cors from 'cors'
 import routes from './routes/index.mjs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 
 const app = express();
 const PORT = process.env.PORT || 3200;
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // allow cors
 app.use(cors())
 
-app.use('/', routes);
+app.use(express.static(path.join(__dirname,'../client/dist')))
 
-/*
-PRODUCTION ONLY
-app.get('*', (req, res) => {
+app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'))
 })
-*/
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
