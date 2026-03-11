e1dfda8e11b683a0a01cdd94addc541d95a49487
ddisanto-langops <1daniel.disanto@gmail.com>
2026-03-11 15:12:38 -0400

    Apply camelCase alias to database return

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -16,7 +16,21 @@ router.get("/api/data", async (req, res) => {
         }
 
         const result = await pool.query(`
-        SELECT * FROM products
+        SELECT 
+            title,
+            productcode AS "productCode",
+            targetlang AS "targetLang",
+            productstatus AS "productStatus",
+            crowdinurl AS "crowdinUrl",
+            trellourl AS "trelloUrl",
+            due,
+            lastactivity AS "lastActivity",
+            published,
+            translationprog AS "translationProg",
+            approvalprog AS "approvalProg",
+            mediatype AS "mediaType",
+            wordcount AS "wordCount"
+        FROM products
         ORDER BY lastactivity DESC NULLS LAST
         `)
         res.json(result.rows)
