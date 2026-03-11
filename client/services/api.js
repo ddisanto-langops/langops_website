a29012f40d2319eaace0abb6552047c21920455f
ddisanto-langops <1daniel.disanto@gmail.com>
2026-03-11 08:34:14 -0400

    modify client-side API for production

--- client/services/api.js
+++ client/services/api.js
@@ -1,5 +1,5 @@
 export async function fetchProducts() {
-    const response = await fetch("http://localhost:3200/api/data")
+    const response = await fetch("/api/data")
     if (!response.ok) throw new Error('Failed to fetch products')
     return response.json()
 }
