fa689006222cc2bc5e08a9fe86c00ec70495ba3c
ddisanto-langops <1daniel.disanto@gmail.com>
2026-03-12 22:33:03 -0400

    Update fetchCompletions for queries

--- client/services/api.js
+++ client/services/api.js
@@ -4,8 +4,19 @@ export async function fetchProducts() {
     return response.json()
 }
 
-export async function fetchCompletions(params) {
-    const response = await fetch(`/api/completions/${params}`)
+export async function fetchCompletions(filters) {
+    const params = new URLSearchParams()
+
+    if (filters.lang) params.append('lang', filters.lang)
+    if (filters.code) params.append('code', filters.code)
+    if (filters.group) params.append('group', filters.group) // This is an array, how to handle it and where?
+    if (filters.from) params.append('from', filters.from)
+    if (filters.to) params.append('to', filters.to)
+
+    const query = params.toString()
+    const url = query ? `/api/completions?${query}` : '/api/completions'
+
+    const response = await fetch(url)
     if (!response.ok) throw new Error("Failed to fetch completions data.")
     return response.json()
 }
